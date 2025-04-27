5bd39ae8b5be3e6775dfe015ef934a2da6aaa3e6
Leonardo Flores Leyva <87454042+leonardofloresleyva@users.noreply.github.com>
2025-04-27 03:31:38 -0700

    Script para crear la base de datos
    Casi se me olvida añadir el script para la creación de la DB.

---
+++ script_creacio_db_252390.sql
@@ -0,0 +1,6 @@
+## Creación de base de datos Ropa_Facil - Proyecto de recuperación de la materia de Bases de datos avanzadas - Segundo parcial
+CREATE DATABASE ROPA_FACIL_252390;
+## Script para eliminar la base de datos (en caso de ser necesario)
+DROP DATABASE ROPA_FACIL_252390;
+## Script para usar la base de datos en MySQL (en caso de ser necesario)
+USE ROPA_FACIL_252390;
